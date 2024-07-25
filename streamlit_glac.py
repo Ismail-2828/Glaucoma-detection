9e8ac9856c0705033e99f5a56747a52fb6ce333f
Ismail-2828 <ismailmoshood1996@gmail.com>
2024-07-25 06:17:26 +0100

    update the code with cvtcolor

--- streamlit_glac.py
+++ streamlit_glac.py
@@ -12,6 +12,7 @@ model = load_model(model_path)
 def preprocess_image(image_path):
     image = cv2.imread(image_path)
     if image is not None:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) 
         image = cv2.resize(image, (224, 224))
         image = np.expand_dims(image, axis=0)
         image = preprocess_input(image)
